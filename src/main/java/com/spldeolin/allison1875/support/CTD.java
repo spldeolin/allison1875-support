13c6d4218840ebf3f17920d02000d7855f0fbcac
Deolin <splendid.deolin@gmail.com>
2022-02-06 10:23:45 +0800

    ADD: CTD

---
+++ src/main/java/com/spldeolin/allison1875/support/CTD.java
@@ -0,0 +1,43 @@
+package com.spldeolin.allison1875.support;
+
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * CTD (Convert-Transformer Detect marker, convert-transformer的转化发现点)
+ *
+ * @author Deolin 2022-02-06
+ */
+public class CTD {
+
+    private final static UnsupportedOperationException e = new UnsupportedOperationException();
+
+    /**
+     * @param from 源Javabean
+     * @param to 目标Javabean的类型
+     * @return 转换后的Javabean
+     */
+    public static <S, D> D of(S from, Class<D> to) {
+        throw e;
+    }
+
+
+    /**
+     * @param from 源Javabean
+     * @param to 目标Javabean（已初始化的）
+     * @return 转换后的Javabean
+     */
+    public static <S, D> D of(S from, D to) {
+        throw e;
+    }
+
+    /**
+     * @param from 多个源Javabean
+     * @param to 目标Javabean的类型
+     * @return 多个转换后的Javabean
+     */
+    public static <S, D> List<D> of(Collection<S> from, Class<D> to) {
+        throw e;
+    }
+
+}
